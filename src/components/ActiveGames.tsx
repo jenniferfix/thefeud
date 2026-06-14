ed74aec463495ef873e05e287310591762f1c39b
Jennifer Fix <codingjennifer@gmail.com>
2026-06-13 19:54:37 -0600

    update active games component

--- src/components/ActiveGames.tsx
+++ src/components/ActiveGames.tsx
@@ -23,27 +23,38 @@ const ActiveGames = ({ userid }: { userid?: string }) => {
   if (isError) return <div>Error: {error.message}</div>;
 
   return (
-    <ScrollArea className="h-[200px]">
-      {data?.map((d) => (
-        <div key={d.id}>
-          <Button variant="link" asChild>
-            <Link to={`/g/$gameInstanceId`} params={{ gameInstanceId: d.id }}>
-              {d.games?.name} started{' '}
-              <ClientOnly>
-                <LocalDateTime value={d.created_at} />
-              </ClientOnly>
-            </Link>
-          </Button>
-          {userid && userid === d.userid && (
-            <Button variant="ghost" size="icon" asChild>
-              <Link to={`/c/$gameInstanceId`} params={{ gameInstanceId: d.id }}>
-                <PlayIcon />
+    <div className="my-12">
+      <h3 className="text-center my-4 text-2xl font-semibold">Public games</h3>
+      <ScrollArea className="h-[200px] bg-card/20 rounded">
+        {!data?.length && (
+          <div className="h-[200px] flex justify-center items-center">
+            Nothing here :(
+          </div>
+        )}
+        {data?.map((d) => (
+          <div key={d.id}>
+            <Button variant="link" asChild>
+              <Link to={`/g/$gameInstanceId`} params={{ gameInstanceId: d.id }}>
+                {d.games?.name} started{' '}
+                <ClientOnly>
+                  <LocalDateTime value={d.created_at} />
+                </ClientOnly>
               </Link>
             </Button>
-          )}
-        </div>
-      ))}
-    </ScrollArea>
+            {userid && userid === d.userid && (
+              <Button variant="ghost" size="icon" asChild>
+                <Link
+                  to={`/c/$gameInstanceId`}
+                  params={{ gameInstanceId: d.id }}
+                >
+                  <PlayIcon />
+                </Link>
+              </Button>
+            )}
+          </div>
+        ))}
+      </ScrollArea>
+    </div>
   );
 };
 
