92f645f899e8ce542389a269a23c578c54cf540a
Jennifer Fix <codingjennifer@gmail.com>
2026-06-16 00:34:50 -0600

    add valkey

---
+++ src/integrations/redis/index.ts
@@ -0,0 +1,68 @@
+import Valkey, { type RedisOptions } from 'iovalkey';
+
+let redis: Valkey | undefined;
+let redisSub: Valkey | undefined;
+
+export const createRedisClient = (params?: RedisOptions) => {
+  redis = new Valkey({
+    host: process.env.REDIS_DOMAIN,
+    enableOfflineQueue: true,
+    retryStrategy: (times) => {
+      return Math.min(times * 50, 30000); // Retry up to 30 seconds
+    },
+    ...params,
+  });
+
+  redis.on('error', (err) => {
+    console.error('Redis connection error: ', err);
+  });
+
+  redis.on('connect', () => {
+    console.log('Redis client connected');
+  });
+
+  redis.on('reconnecting', () => {
+    console.log('Redis client reconnecting');
+  });
+
+  return redis;
+};
+
+export const createRedisSubClient = (params?: RedisOptions) => {
+  redisSub = new Valkey({
+    host: process.env.REDIS_DOMAIN,
+    lazyConnect: true,
+    enableOfflineQueue: true,
+    retryStrategy: (times) => {
+      return Math.min(times * 50, 30000); // Retry up to 30 seconds
+    },
+    ...params,
+  });
+
+  redisSub.on('error', (err) => {
+    console.error('Redis sub connection error: ', err);
+  });
+
+  redisSub.on('connect', () => {
+    console.log('Redis sub client connected');
+  });
+
+  redisSub.on('reconnecting', () => {
+    console.log('Redis sub client reconnecting');
+  });
+
+  return redisSub;
+};
+
+export const getRedisClient = () => {
+  if (redis) return redis;
+  else return createRedisClient();
+};
+
+export const getRedisSubClient = () => {
+  if (redisSub) return redisSub;
+  else return createRedisSubClient();
+};
+
+export type RedisClient = ReturnType<typeof createRedisClient>;
+export type RedisSubClient = ReturnType<typeof createRedisSubClient>;
