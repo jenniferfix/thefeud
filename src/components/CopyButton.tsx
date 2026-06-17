20d5211b30949a104df4cc021b46050ab971adc2
Jennifer Fix <codingjennifer@gmail.com>
2026-06-17 01:03:07 -0600

    create copy button component with hook

---
+++ src/components/CopyButton.tsx
@@ -0,0 +1,38 @@
+import { CircleCheck, Copy } from 'lucide-react';
+import React from 'react';
+import { useCopyToClipboard } from '#/hooks/useCopyToClipboard';
+import { Button } from '@/components/ui/button';
+
+export const CopyButton = ({
+  copyValue,
+  ...props
+}: { copyValue?: string | null } & React.ComponentProps<typeof Button>) => {
+  const { copy, isCopied, reset } = useCopyToClipboard();
+  const [showCheck, setShowCheck] = React.useState(false);
+
+  const handleClick = React.useCallback(() => {
+    if (!copyValue) return;
+    copy(copyValue);
+  }, [copyValue, copy]);
+
+  React.useEffect(() => {
+    if (!isCopied) return;
+    setShowCheck(true);
+    const timeoutId = setTimeout(() => {
+      setShowCheck(false);
+    }, 3000);
+    return () => clearTimeout(timeoutId);
+  });
+
+  return (
+    <Button
+      size="icon"
+      variant="ghost"
+      disabled={!copyValue}
+      onClick={handleClick}
+      {...props}
+    >
+      {showCheck ? <CircleCheck /> : <Copy />}
+    </Button>
+  );
+};
