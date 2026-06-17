7b0ce1969118aca347d32e13338c1ae6c44af8f4
Jennifer Fix <codingjennifer@gmail.com>
2026-06-17 01:00:43 -0600

    add my getErrorMessage func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,8 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const normalizeToArray = <T>(input: T | T[]): T[] => {
@@ -11,5 +11,22 @@ export const normalizeToArray = <T>(input: T | T[]): T[] => {
     return input;
   } else {
     return [input];
+  }
+};
+
+export const getErrorMessage = (error: unknown) => {
+  if (error instanceof Error) {
+    return error.message;
+  } else if (typeof error === 'string') {
+    return error;
+  } else if (
+    typeof error === 'object' &&
+    error !== null &&
+    'message' in error &&
+    typeof error.message === 'string'
+  ) {
+    return error.message;
+  } else {
+    return 'Unknown error';
   }
 };
